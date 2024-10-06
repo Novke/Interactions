7062dc6de850ff6bdc462c2b562a63932b4a484e
Novke <novke.brate@gmail.com>
2024-10-06 14:08:54 +0200

    person module & new person component

--- interactions-angular/src/app/app-routing.module.ts
+++ interactions-angular/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewPersonComponent } from './person/new-person/new-person.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'person/new', component: NewPersonComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ interactions-angular/src/app/person/person.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NewPersonComponent } from './new-person/new-person.component';
+
+
+
+@NgModule({
+  declarations: [
+    NewPersonComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PersonModule { }
