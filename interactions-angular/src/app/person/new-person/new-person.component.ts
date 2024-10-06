82e8f3647f5b1b1357caa659e5a190f2d03336f7
Novke <novke.brate@gmail.com>
2024-10-06 15:11:16 +0200

    person service & save person

--- interactions-angular/src/app/person/new-person/new-person.component.ts
+++ interactions-angular/src/app/person/new-person/new-person.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PersonService } from '../person.service';
+import { Person } from 'src/app/models/person';
 
 @Component({
   selector: 'app-new-person',
@@ -11,8 +13,25 @@ export class NewPersonComponent {
   newPersonLastname = ""
   newPersonDescription = ""
 
+  constructor(private personService: PersonService){
+  }
+
   savePerson(): void{
     console.log("Saving person ", this.newPersonName, this.newPersonLastname)
+    
+    const person = new Person();
+    person.name=this.newPersonName;
+    person.lastname=this.newPersonLastname;
+    person.description=this.newPersonDescription;
+    
+    this.personService.savePerson(person).subscribe(
+      response => {
+      console.log("Saved succesfully! ID =", response.id)
+      },
+      error => {
+        console.error("Error saving person", error)
+      }
+    )
   }
 
 }
---
+++ interactions-angular/src/app/person/person.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Person } from '../models/person';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PersonService {
+
+  apiUrl = environment.apiUrl + "/person";
+
+  constructor(private http: HttpClient) { 
+  }
+
+  savePerson(person: Person): Observable<Person> {
+    return this.http.post<Person>(this.apiUrl, person);
+  }
+}
